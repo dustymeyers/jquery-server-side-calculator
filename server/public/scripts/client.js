ed8e6f52a08639c9cc3e26a663d9f716fc18d57f
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-02-06 13:52:59 -0600

    appens most previous submission's answer to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,6 +9,7 @@ function onReady() {
     method: 'GET',
   }).then(function (operationHistory) {
     console.log('This is our response', operationHistory);
+    let lastIndex = operationHistory.length - 1;
     // loop through the operationHistory array
     $('#operation-history-output').empty();
     for (let operation of operationHistory) {
@@ -17,6 +18,10 @@ function onReady() {
         <li>${operation.firstOperand} ${operation.operator} ${operation.secondOperand} = ${operation.solution}</li>
       `);
     }
+    // we also need to append the most recent operation answer to the DOM
+    let lastSolution = operationHistory[lastIndex].solution;
+    $('#answer-output').empty();
+    $('#answer-output').append(lastSolution);
   });
 }
 // create a function that gets the data on submit
